f9d11f8516fef1cbf80796de4079ae6355d92dee
PauloB <47363007+PauloBDev@users.noreply.github.com>
2024-09-23 14:42:17 +0100

    fonts and appBar changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:diary/bloc/to_do_list_bloc.dart';
-import 'package:diary/to_do_list_page.dart';
+import 'package:diary/pages/home.dart';
+import 'package:diary/pages/toDoList.dart';
 import 'package:diary/widgets/clock_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -27,11 +28,11 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Diary',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color(0x00005073),
-              surface: const Color(0x0000415e)),
-          useMaterial3: true,
-        ),
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: const Color(0x00005073),
+                surface: const Color(0x0000415e)),
+            useMaterial3: true,
+            fontFamily: 'Poppins'),
         home: MyHomePage(title: 'Diary', time: DateTime.now()),
       ),
     );
@@ -50,83 +51,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        elevation: 10,
-        shadowColor: Colors.black,
-        title:
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-          Text(
-            widget.title,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const ClockWidget(optionTime: TimeOption.time)
-        ]),
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Center(
-                  child: FittedBox(
-                    fit: BoxFit.fill,
-                    child: Text(
-                      'Control your life!',
-                      style: TextStyle(fontSize: 40, color: Colors.white),
-                    ),
-                  ),
-                ),
-                MouseRegion(
-                  cursor: SystemMouseCursors.click,
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                        padding: const EdgeInsets.only(right: 20.0),
-                        child: const Icon(Icons.refresh_rounded)),
-                  ),
-                )
-              ],
-            ),
-            Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                  color: Colors.black45,
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              height: 500,
-              child: const Center(
-                child: Text('GRAPH HERE'),
-              ),
-            ),
-            MouseRegion(
-              cursor: SystemMouseCursors.click,
-              child: GestureDetector(
-                onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => ToDoListPage(
-                          title: widget.title,
-                          time: widget.time,
-                        ))),
-                child: const Card(
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 100,
-                    child: Center(
-                      child: Text(
-                        "Check your To-DO's",
-                        style: TextStyle(fontSize: 32),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return HomePage(title: widget.title, time: widget.time);
   }
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,113 @@
+import 'package:diary/pages/toDoList.dart';
+import 'package:diary/widgets/clock_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title, required this.time});
+  final String title;
+  final DateTime time;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black87,
+        elevation: 0.0,
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+                margin: EdgeInsets.only(right: 20),
+                child: Icon(Icons.settings)),
+          )
+        ],
+      ),
+      body: Container(
+        height: double.infinity,
+        color: Colors.black87,
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  FittedBox(
+                    fit: BoxFit.fill,
+                    child: Text(
+                      'Control your life!',
+                      style: TextStyle(
+                          fontSize: screenWidth * 0.08, color: Colors.white),
+                    ),
+                  ),
+                  MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                          padding: const EdgeInsets.only(right: 20.0),
+                          child: const Icon(Icons.refresh_rounded)),
+                    ),
+                  )
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                child: const Center(
+                  child: Text(
+                    'GRAPH HERE',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: GestureDetector(
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => ToDoListPage(
+                        title: widget.title,
+                        time: widget.time,
+                      ),
+                    ),
+                  ),
+                  child: Card(
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 100,
+                      child: Center(
+                        child: Text(
+                          "Check your To-DO's",
+                          style: TextStyle(
+                              fontSize: screenWidth * 0.08,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
